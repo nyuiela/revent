4435de7bda71a2fb675768e0e5556593314441f4
kaleel <pixelhubster@gmail.com>
2025-09-05 01:54:37 +0000

    feat: footer bar

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import { type ReactNode } from "react";
-import { base } from "wagmi/chains";
+import { baseSepolia } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { ThemeProvider } from "next-themes";
 
 export function Providers(props: { children: ReactNode }) {
-  return (
-    <MiniKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
-      config={{
-        appearance: {
-          mode: "auto",
-          theme: "mini-app-theme",
-          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-          logo: process.env.NEXT_PUBLIC_ICON_URL,
-        },
-      }}
-    >
-      {props.children}
-    </MiniKitProvider>
-  );
+   return (
+      <ThemeProvider
+         attribute="class"
+         defaultTheme="system"
+         enableSystem
+      // disableTransitionOnChange
+      >
+         <MiniKitProvider
+            apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+            chain={baseSepolia} // change to baseSepolia later
+            config={{
+               appearance: {
+                  mode: "dark",
+                  theme: "mini-app-theme",
+                  name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+                  logo: process.env.NEXT_PUBLIC_ICON_URL,
+               },
+            }}
+         >
+            {props.children}
+         </MiniKitProvider>
+      </ThemeProvider>
+   );
 }
